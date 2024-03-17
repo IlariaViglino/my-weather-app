f789a770f002bcfa9bd7e11af80643247d293cae
IlariaViglino <ilaria.viglino@yahoo.it>
2024-03-17 11:26:22 +0100

    final touches

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 //change background
-function changeBackground(event) {
-  event.preventDefault();
+//function changeBackground(event) {
+// event.preventDefault();
+// let body = document.querySelector("body");
+// body.classList.add("city-background");
+//}
+//let submit = document.querySelector("#submit-button");
+//submit.addEventListener("submit", changeBackground);
+//change background
+function changeBackground() {
   let body = document.querySelector("body");
   body.classList.add("city-background");
 }
